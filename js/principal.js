274d3f444a34f19b7b68f5316ab93318f47d1ebe
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-12-04 23:28:48 -0300

    Validando os campos de entrada.

--- js/principal.js
+++ js/principal.js
@@ -4,13 +4,31 @@ titulo.textContent = "Sistema de Nutrição Online Javascript";
 let subtitulo = document.querySelector(".subtitulo");
 subtitulo.textContent = "Pacientes";
 
-let paciente = document.querySelector(".paciente");
 
+let paciente = document.querySelector(".paciente");
 let peso = paciente.querySelector(".peso").textContent;
 let altura = paciente.querySelector(".altura").textContent;
-
-let imc = peso/(Math.pow(altura,2));
-
 let indice = paciente.querySelector(".indice");
-indice.textContent = imc.toFixed(2);
+
+let pesoEhValido = true;
+let alturaEhValida = true;
+
+
+if(peso < 0 || peso > 500){
+  pesoEhValido = false;
+  indice.textContent = "Peso inválido!";
+}
+
+if( altura < 0 |! altura > 3){
+    alturaEhValida = false;
+    indice.textContent = "Altura inválida!";
+}
+
+if(pesoEhValido && alturaEhValida){
+    let imc = peso/(Math.pow(altura,2));
+    indice.textContent = imc.toFixed(2);
+}
+
+    
+
 
