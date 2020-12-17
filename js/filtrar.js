cb3f6bb1cb4c7796b72e8e9c631a6844781c988c
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-12-17 00:28:57 -0300

    Melhorando a funcão de filtro de pacientes com RegExp.

--- js/filtrar.js
+++ js/filtrar.js
@@ -1,20 +1,19 @@
 let filtro = document.querySelector("#filtro");
 
 filtro.addEventListener("input", function() {
-	console.log(this.value);
 	let pacientes = document.querySelectorAll(".paciente");
-
     if(this.value.length > 0) {
 		pacientes.forEach(paciente => {
 			let tdnome = paciente.querySelector(".nome");
 			let nome = tdnome.textContent;
-			console.log(nome);
-			if(nome != this.value) {
+	        let expressao = new RegExp(this.value,"i");		
+			if(!expressao.test(nome)) {
 				paciente.classList.add("hidden");
 			} else {
 				paciente.classList.remove("hidden");
 			}
-		});
+        });
+        
 	} else {
 		pacientes.forEach(paciente => {
 			paciente.classList.remove("hidden");
